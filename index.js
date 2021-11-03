346c9698cd5b56806218af9812ddeee301cb8081
JibronHarris <Jibron.Harris@gmail.com>
2021-11-02 22:03:11 -0400

    linked css stuff and added some

---
+++ index.js
@@ -0,0 +1,12 @@
+const html = require("html-template-tag");
+
+module.exports = index => html `<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="/views/style.css" />
+<head>
+<body>
+  <h1>TEST HEADER</h1>
+</body>
+</html>
+`;
