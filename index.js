6feadb02f01120a50df5e0f61b2c39a7d6273c17
MichaelClapso <michael.clapso@yahoo.com>
2021-11-04 13:23:14 -0400

    installed sass

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const html = require("html-template-tag");
 
-module.exports = index => html `<!DOCTYPE html>
+module.exports = index => html`<!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="/style.css" />
+  <link rel="stylesheet" href="stylesheets/style.css" />
 <head>
 <body>
-  <h1>TEST HEADER</h1>
+  <h1>TEST HEADERssss</h1>
 </body>
 </html>
 `;
